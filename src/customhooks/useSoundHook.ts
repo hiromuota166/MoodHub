5c8adcaef60511b9b821257e7c5238645106ab3f
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-15 01:43:48 +0900

    閾値を下げたり、usecallback使ったり

--- src/customhooks/useSoundHook.ts
+++ src/customhooks/useSoundHook.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const useSoundHook = () => {
 	const [isSoundOn, setIsSoundOn] = useState(false);
@@ -44,38 +44,43 @@ export const useSoundHook = () => {
 		// }
 	};
 
-	useEffect(() => {
-		const playSound = () => {
-			const now = Date.now();
-			if (isSoundOn && now - lastPlayedTime.current >= 150) {
-				// 1秒以上経過しているか確認
-				const audio = new Audio("/maracas-sound.wav");
-				audio.play();
-				lastPlayedTime.current = now; // 最後に再生した時刻を更新
-			}
-		};
-		const handleShake = (e: DeviceMotionEvent) => {
+	const playSound = useCallback(() => {
+		const audio = new Audio("/maracas-sound.wav");
+		audio.play();
+	}, []);
+
+	const handleShake = useCallback(
+		(e: DeviceMotionEvent) => {
 			const ax = e.acceleration?.x || 0;
 			const ay = e.acceleration?.y || 0;
 			const az = e.acceleration?.z || 0;
+
 			if (e.acceleration && ax && ay && az) {
 				setAccelerationX(ax);
 				setAccelerationY(ay);
 				setAccelerationZ(az);
 			}
-			const shakeThreshold = 15;
 
-			if (Math.abs(ax) > shakeThreshold || Math.abs(ay) > shakeThreshold || Math.abs(az) > shakeThreshold) {
+			const shakeThreshold = 5;
+			const magnitude = Math.sqrt(ax ** 2 + ay ** 2 + az ** 2);
+
+			if (magnitude > shakeThreshold) {
 				playSound();
 			}
-		};
+		},
+		[setAccelerationX, setAccelerationY, setAccelerationZ, playSound]
+	);
 
-		const handleSwipe = () => {
-			// Swipe detection logic
-			// ...
+	const handleSwipe = useCallback(() => {
+		const now = Date.now();
+		if (isSoundOn && now - lastPlayedTime.current >= 150) {
+			// 1秒以上経過しているか確認
 			playSound();
-		};
+			lastPlayedTime.current = now; // 最後に再生した時刻を更新
+		}
+	}, [playSound, isSoundOn]);
 
+	useEffect(() => {
 		if (isSoundOn && isPermissionGranted) {
 			window.addEventListener("devicemotion", handleShake);
 			window.addEventListener("touchmove", handleSwipe);
@@ -85,7 +90,7 @@ export const useSoundHook = () => {
 			window.removeEventListener("devicemotion", handleShake);
 			window.removeEventListener("touchmove", handleSwipe);
 		};
-	}, [isSoundOn]);
+	}, [isSoundOn, isPermissionGranted, handleShake, handleSwipe]);
 
 	return {
 		isSoundOn,
