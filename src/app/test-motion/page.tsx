f6e88994de93a7b3d07419dd961afafcfda235f5
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-17 17:41:32 +0900

    slack over flow参考にしてみた

--- src/app/test-motion/page.tsx
+++ src/app/test-motion/page.tsx
@@ -4,9 +4,38 @@ import useSoundHook from "@/customhooks/useSoundHook";
 const Page = () => {
 	const { isSoundOn, setIsSoundOn, isPermissionGranted, requestPermission, acceleration, playSound } = useSoundHook();
 
+	function playVoice(): Promise<HTMLAudioElement> {
+		const languageCode = "en-US";
+		const voice = "NEUTRAL";
+
+		// const url = createSpeechUrl(text, languageCode, voice);
+		// Substitute for this SO code snippet:
+
+		// Instantiate the audio element with the source URL
+		// so that it can stream the audio data as early as possible
+		// (without waiting for the entire "file" to buffer)
+		const audio = new Audio("/maracas-sound.mp3");
+
+		// Return a promise with the result of attempting playback
+		// after enough streaming data has been downloaded
+		return new Promise<HTMLAudioElement>((resolve, reject) =>
+			audio.addEventListener("canplaythrough", () =>
+				audio
+					.play()
+					.then(() => resolve(audio))
+					.catch(reject)
+			)
+		);
+	}
+
 	return (
 		<div>
 			<div className=''>
+				<div>
+					<button className='m-4' onClick={() => playVoice()}>
+						play Sound
+					</button>
+				</div>
 				<div>
 					<button
 						className='m-4'
