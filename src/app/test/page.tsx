58a6e08cf845f6ca1f967d20b3cec7cc6022d467
hiromuota166 <otahiromu166@gmail.com>
2023-10-18 11:35:53 +0900

    buildのエラー対処しました

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 import GoogleLoginBtn from '@/components/GoogleLoginBtn'
 import React from 'react'
 
-function page() {
+function Page() {
   return (
     <>
       <GoogleLoginBtn/>
@@ -9,4 +9,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
