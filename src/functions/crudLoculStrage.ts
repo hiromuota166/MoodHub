aa9c737d4299effada79cf618a45bd2b9db42c92
hiromuota166 <otahiromu166@gmail.com>
2023-10-18 11:14:49 +0900

    localstrageの変更

--- src/functions/crudLoculStrage.ts
+++ src/functions/crudLoculStrage.ts
@@ -1,47 +1,56 @@
 // ローカルストレージにデータを追加する関数
 const addToLocalStorage = (key: "genre" | "era", value: string[]): void => {
-    try {
+  try {
+    if (typeof window !== 'undefined') {
       localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.error("Failed to save to localStorage:", error);
     }
-  };
-  
-  // ローカルストレージからデータを取得する関数
-  const getFromLocalStorage = (key: "genre" | "era"): string[] | null => {
-    try {
+  } catch (error) {
+    console.error("Failed to save to localStorage:", error);
+  }
+};
+
+// ローカルストレージからデータを取得する関数
+const getFromLocalStorage = (key: "genre" | "era"): string[] | null => {
+  try {
+    if (typeof window !== 'undefined') {
       const storedData = localStorage.getItem(key);
       return storedData ? JSON.parse(storedData) : null;
-    } catch (error) {
-      console.error("Failed to get data from localStorage:", error);
+    }
+    else {
       return null;
     }
-  };
-  
-  // ローカルストレージからデータを削除する関数
-  const removeFromLocalStorage = (key: "genre" | "era"): void => {
-    try {
-      localStorage.removeItem(key);
-    } catch (error) {
-      console.error("Failed to remove data from localStorage:", error);
-    }
-  };
-  
-  // 使い方の例
-  const testLocalStorageFunctions = (): void => {
-    // データの追加
-    addToLocalStorage("genre", ["rock", "pop"]);
-    addToLocalStorage("era", ["80s", "90s"]);
-  
-    // データの取得
-    console.log(getFromLocalStorage("genre"));  // ["rock", "pop"]
-    console.log(getFromLocalStorage("era"));    // ["80s", "90s"]
-  
-    // データの削除
-    removeFromLocalStorage("genre");
-    console.log(getFromLocalStorage("genre"));  // null
-  };
-  
-  testLocalStorageFunctions();
+  } catch (error) {
+    console.error("Failed to get data from localStorage:", error);
+    return null;
+  }
+};
 
-  export {addToLocalStorage, getFromLocalStorage, removeFromLocalStorage};
+// ローカルストレージからデータを削除する関数
+const removeFromLocalStorage = (key: "genre" | "era"): void => {
+  try {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem(key);
+    }
+  } catch (error) {
+    console.error("Failed to remove data from localStorage:", error);
+  }
+};
+
+// 使い方の例
+const testLocalStorageFunctions = (): void => {
+  // データの追加
+  addToLocalStorage("genre", ["rock", "pop"]);
+  addToLocalStorage("era", ["80s", "90s"]);
+
+  // データの取得
+  console.log(getFromLocalStorage("genre"));  // ["rock", "pop"]
+  console.log(getFromLocalStorage("era"));    // ["80s", "90s"]
+
+  // データの削除
+  removeFromLocalStorage("genre");
+  console.log(getFromLocalStorage("genre"));  // null
+};
+
+testLocalStorageFunctions();
+
+export { addToLocalStorage, getFromLocalStorage, removeFromLocalStorage };
