8c08286fef397c260ec2588fffd6f190de68a199
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-18 13:48:16 +0900

    add light maracas

---
+++ src/app/maracas/light/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import useSoundHook from "@/customhooks/useSoundHook";
+import Image from "next/image";
+const Page = () => {
+    const { isSoundOn, setIsSoundOn, isDevicemotionPermissionGranted, requestPermission } = useSoundHook();
+    return (
+        <div>
+            <div>light maracas</div>
+            <div className='absolute top-0 right-0 z-10'>
+                <button onClick={() => setIsSoundOn(!isSoundOn)}>{isSoundOn ? "Stop" : "Start"} Sound</button>
+                {!isDevicemotionPermissionGranted || true ? <button onClick={requestPermission}>Enable Motion</button> : <></>}
+            </div>
+            <div className="w-[60%] h-[60%] m-auto">
+                <Image src={"/music_maracas.webp"} alt="マラカスの画像" width={400} height={382} layout="responsive"></Image>
+
+            </div>
+        </div>
+    )
+}
+
+export default Page;
--- src/app/maracas/page.tsx
+++ src/app/maracas/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 const Page = () => {
 	return (
 		<div>
+			<Link href={"/maracas/light"}>軽量マラカス</Link>
 			<Link href={"/maracas/normal"}>ノーマルマラカス</Link>
 			<Link href={"/maracas/special"}>スペシャルマラカス</Link>
 		</div>
