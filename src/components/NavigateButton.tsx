9ac7f1a0311da92bc681edb378d2fdfdf73a7d6e
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-19 11:08:53 +0900

    handle関数作った

--- src/components/NavigateButton.tsx
+++ src/components/NavigateButton.tsx
@@ -1,19 +1,37 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 interface NavigationButtonProps {
-    href: string;
-    label: string;
-    onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+	href: string;
+	label: string;
+	onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+	linkDisabled?: boolean;
 }
 
 const NavigateButton = (props: NavigationButtonProps) => {
-    const { href, label, onClick } = props;
-    return (
-        <Link href={href} onClick={() => onClick} className='block bg-background text-font text-lg text-3xl m-4 p-8 py-10 w-fit rounded-3xl shadow-boxOut'>
-            {label}
-        </Link>
-
-    )
-}
+	const { href, label, onClick, linkDisabled } = props;
+	if (linkDisabled && onClick)
+		return (
+			<a
+				href='/'
+				onClick={(e) => {
+					onClick(e);
+				}}
+			>
+				<div className='block bg-background text-font text-xl m-4 p-8 py-10 w-fit rounded-3xl shadow-boxOut'>
+					{label}
+				</div>
+			</a>
+		);
+	return (
+		<Link
+			href={href}
+			onClick={() => onClick}
+			className='block bg-background text-font text-xl m-4 p-8 py-10 w-fit rounded-3xl shadow-boxOut'
+			aria-disabled
+		>
+			{label}
+		</Link>
+	);
+};
 
 export default NavigateButton;
