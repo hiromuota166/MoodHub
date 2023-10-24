12f4ded298213b6c6cb10239a04d48ee2140be5a
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-24 15:26:18 +0900

    thre sceneの最適化

--- src/customhooks/threeCustomhooks/useThreeScene.ts
+++ src/customhooks/threeCustomhooks/useThreeScene.ts
@@ -4,11 +4,15 @@ import { useEffect, useRef, useState } from "react";
 export const useThreeScene = () => {
 	console.log("useThreeScene");
 	const mountRef = useRef<HTMLDivElement>(null);
-	const [renderer, setRenderer] = useState<THREE.WebGLRenderer | null>(null);
+	const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
 	const sceneRef = useRef<THREE.Scene | null>(null);
 
 	useEffect(() => {
-		setRenderer(new THREE.WebGLRenderer());
+		// rendererが未初期化の場合のみ初期化
+		if (!rendererRef.current) {
+			console.log("renderer is not initialized");
+			rendererRef.current = new THREE.WebGLRenderer();
+		}
 
 		// sceneが未初期化の場合のみ初期化
 		if (!sceneRef.current) {
@@ -21,8 +25,7 @@ export const useThreeScene = () => {
 
 	return {
 		mountRef,
-		renderer,
+		renderer: rendererRef.current,
 		scene: sceneRef.current,
 	};
 };
-
