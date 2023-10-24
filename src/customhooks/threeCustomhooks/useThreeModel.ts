576d527a6f874452c521016bc48074535ac85677
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-24 17:55:11 +0900

    マラカスの大きさ調整

--- src/customhooks/threeCustomhooks/useThreeModel.ts
+++ src/customhooks/threeCustomhooks/useThreeModel.ts
@@ -12,14 +12,14 @@ export const useThreeModel = (scene: THREE.Scene | null, mode: "normal" | "speci
 
 	if (mode === "normal") {
 		loadModel("/maracas-wood.glb", (gltf) => {
-			gltf.scene.position.set(0, 1.5, 0);
-			gltf.scene.scale.set(1.5, 1.5, 1.5);
+			gltf.scene.position.set(0, 1.7, 0);
+			gltf.scene.scale.set(1.8, 1.8, 1.8);
 			scene.add(gltf.scene);
 		});
 	} else if (mode === "special") {
 		loadModel("/maracas-gold.glb", (gltf) => {
-			gltf.scene.scale.set(1.5, 1.5, 1.5);
-			gltf.scene.position.set(0, 1.5, 0);
+			gltf.scene.scale.set(1.8, 1.8, 1.8);
+			gltf.scene.position.set(0, 1.7, 0);
 			scene.add(gltf.scene);
 		});
 	}
