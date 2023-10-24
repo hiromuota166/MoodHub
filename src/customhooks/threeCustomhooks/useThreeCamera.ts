f8d3bd6087bdbe1b5954078b62f17a5173fd81c4
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-24 15:32:15 +0900

    three cameraの最適化

--- src/customhooks/threeCustomhooks/useThreeCamera.ts
+++ src/customhooks/threeCustomhooks/useThreeCamera.ts
@@ -1,16 +1,22 @@
 // useThreeCamera.ts
 import * as THREE from "three";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useThreeCamera = () => {
 	console.log("useThreeCamera");
 	const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>(null);
+	const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
 
 	useEffect(() => {
-		const newCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		newCamera.position.set(0, 0, 5);
-		setCamera(newCamera);
+		// cameraが未初期化の場合のみ初期化
+		if (!cameraRef.current) {
+			console.log("camera is not initialized");
+			const newCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+			newCamera.position.set(0, 0, 5);
+			cameraRef.current = newCamera;
+			setCamera(newCamera); // 状態を更新して再レンダリングをトリガーする
+		}
 	}, []);
 
-	return camera;
+	return camera; // useStateで管理されているcameraを返す
 };
