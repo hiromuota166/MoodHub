6e18c0aac18accb35d443275fcffda9bf5cf0292
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-10-26 10:19:09 +0900

    title設定と、favicon設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n: {
+        locales: ['ja', 'en'], // 使用する言語を追加
+        defaultLocale: 'ja', // デフォルトの言語を設定
+    },
+}
 
 module.exports = nextConfig
