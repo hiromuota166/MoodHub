652c9b196d3229c0392f3a1d322c69ed4aeec0a0
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-11-04 19:19:37 +0900

    del eng

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n: {
-        locales: ['ja', 'en'], // 使用する言語を追加
+        locales: ['ja'], // 使用する言語を追加
         defaultLocale: 'ja', // デフォルトの言語を設定
     },
 }
