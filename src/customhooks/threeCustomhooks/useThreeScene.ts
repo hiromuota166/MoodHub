21b0f34a026bc7450d9974b503436d28e16f2ae5
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-11-16 16:32:22 +0900

    useThreeSceneの最適化

--- src/customhooks/threeCustomhooks/useThreeScene.ts
+++ src/customhooks/threeCustomhooks/useThreeScene.ts
@@ -1,50 +1,36 @@
 import * as THREE from "three";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const useThreeScene = () => {
 	const mountRef = useRef<HTMLDivElement>(null);
 	const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
 	const sceneRef = useRef<THREE.Scene | null>(null);
-	const [dimensions, setDimensions] = useState({ width: 0, height: 0 }); // 初期値を0に設定
 
 	useEffect(() => {
-		setDimensions({
-			width: document.documentElement.clientWidth,
-			height: window.innerHeight,
-		});
-
-		// rendererが未初期化の場合のみ初期化
+		// Rendererの初期化
 		if (!rendererRef.current) {
-			const newRenderer = new THREE.WebGLRenderer();
-			newRenderer.setSize(dimensions.width, dimensions.height);
-			rendererRef.current = newRenderer;
+			rendererRef.current = new THREE.WebGLRenderer();
+			rendererRef.current.setSize(window.innerWidth, window.innerHeight);
 		}
 
-		// sceneが未初期化の場合のみ初期化
+		// Sceneの初期化
 		if (!sceneRef.current) {
-			const newScene = new THREE.Scene();
-			newScene.background = new THREE.Color("#000");
-			sceneRef.current = newScene;
+			sceneRef.current = new THREE.Scene();
+			sceneRef.current.background = new THREE.Color("#000");
 		}
-		// resizeイベントリスナーを追加
-		const handleResize = () => {
-			const width = document.documentElement.clientWidth;
-			const height = window.innerHeight;
-			setDimensions({ width, height });
 
+		// ウィンドウのリサイズイベントリスナーを設定
+		const handleResize = () => {
 			if (rendererRef.current) {
-				rendererRef.current.setSize(width, height);
+				rendererRef.current.setSize(document.documentElement.clientWidth, window.innerHeight);
 			}
 		};
-
 		window.addEventListener("resize", handleResize);
 
 		return () => {
-			// cleanup: remove the event listener when the component is unmounted
 			window.removeEventListener("resize", handleResize);
 		};
-	}, [dimensions.height, dimensions.width]);
-
+	}, []);
 	return {
 		mountRef,
 		renderer: rendererRef.current,
