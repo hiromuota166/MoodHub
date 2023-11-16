4980b4f9f18710fd2c354b8016c5dec8dadfbcd8
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-11-16 10:40:52 +0900

    変更点の一時的こ宇

--- src/customhooks/threeCustomhooks/useThreeModel.ts
+++ src/customhooks/threeCustomhooks/useThreeModel.ts
@@ -1,26 +1,39 @@
-// useThreeModel.ts
+import { useEffect } from "react";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import * as THREE from "three";
 
 export const useThreeModel = (scene: THREE.Scene | null, mode: "normal" | "special") => {
-	if (!scene) return;
-	const loader = new GLTFLoader();
+	useEffect(() => {
+		if (!scene) return;
 
-	const loadModel = (url: string, onLoadCallback: (gltf: any) => void) => {
-		loader.load(url, onLoadCallback, undefined, (error) => console.error("An error happened", error));
-	};
+		const loader = new GLTFLoader();
+		const loadModel = (url: string, onLoadCallback: (gltf: any) => void) => {
+			loader.load(url, onLoadCallback, undefined, (error) => console.error("An error happened", error));
+		};
 
-	if (mode === "normal") {
-		loadModel("/maracas-wood-v2.glb", (gltf) => {
-			gltf.scene.position.set(0, 1.7, 0);
-			gltf.scene.scale.set(1.8, 1.8, 1.8);
-			scene.add(gltf.scene);
-		});
-	} else if (mode === "special") {
-		loadModel("/maracas-gold-v2.glb", (gltf) => {
-			gltf.scene.scale.set(1.8, 1.8, 1.8);
-			gltf.scene.position.set(0, 1.7, 0);
-			scene.add(gltf.scene);
-		});
-	}
+		let model: THREE.Object3D;
+
+		if (mode === "normal") {
+			loadModel("/maracas-wood-v2.glb", (gltf) => {
+				model = gltf.scene;
+				model.position.set(0, 1.7, 0);
+				model.scale.set(1.8, 1.8, 1.8);
+				scene.add(model);
+			});
+		} else if (mode === "special") {
+			loadModel("/maracas-gold-v2.glb", (gltf) => {
+				model = gltf.scene;
+				model.scale.set(1.8, 1.8, 1.8);
+				model.position.set(0, 1.7, 0);
+				scene.add(model);
+			});
+		}
+
+		return () => {
+			// コンポーネントのクリーンアップ時にモデルをシーンから削除
+			if (model) {
+				scene.remove(model);
+			}
+		};
+	}, [scene, mode]);
 };
