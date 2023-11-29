3b210ae00cafd0dc006bc7576987b19975f0379b
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-11-29 18:07:35 +0900

    format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import GoogleLoginBtn from "./GoogleLoginBtn"
+import GoogleLoginBtn from "./GoogleLoginBtn";
 
 const Header = () => {
   return (
     <header className="shadow-boxOut h-20 flex items-center justify-left ">
-        <h1 className="p-4 pt-6 text-3xl text-font">MoodHub</h1>
-        <GoogleLoginBtn />
+      <h1 className="p-4 pt-6 text-3xl text-font">MoodHub</h1>
+      <GoogleLoginBtn />
     </header>
-    )
-}
+  );
+};
 
 export default Header;
