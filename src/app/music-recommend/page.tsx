b959595a8291be1c09d74d83a154eb74bd542bcb
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-12-05 14:15:32 +0900

    mosic recommendページを整理

--- src/app/music-recommend/page.tsx
+++ src/app/music-recommend/page.tsx
@@ -1,80 +1,78 @@
 "use client";
-import React from "react";
+import React, { Suspense, useState } from "react";
 import NeumourList from "@/components/NeumorList";
 import ShowRoomID from "@/components/ShowRoomID";
 import ModalWhole from "@/components/ModalWhole";
 import { useSearchParams } from "next/navigation";
 import IsLoading from "@/components/IsLoading";
-import { auth } from "@/lib/firebase";
 import useMusicRecommendPageData from "@/hooks/useMusicRecommendPageData";
-import { Song } from "@/lib/apollo/gql/graphql";
+import { useAuth } from "@/context/auth";
 
 interface SongListProps {
-  songsData: { song: Song[] } | undefined;
+  roomID: number;
+  userID: string;
 }
 
-const SongList = ({ songsData }: SongListProps) => {
-  if (!songsData || songsData.song.length === 0) return <p>曲がありません。</p>;
-  const songs = songsData.song;
-  const songNames = songs.map((song) => song.songName);
+const SongList = (props: SongListProps) => {
+  const { userID, roomID } = props;
+  const [loading, setloading] = useState(true);
+  const { Song, handleUpdateCategories } = useMusicRecommendPageData(
+    userID,
+    roomID
+  );
+  const error = Song.error;
+  const songs = Song.data?.song;
+  const songNames = songs ? songs.map((song) => song.songName) : [];
+
+  const handleModalUpdate = (categories: string[]) => {
+    setloading(true);
+    handleUpdateCategories(categories).then(() => {
+      setloading(false);
+    });
+  };
+
   return (
     <>
-      <NeumourList listItems={songNames} />
+      <ModalWhole default={true} handleUpdateCategories={handleModalUpdate} />
+      <ShowRoomID roomID={String(roomID)} />
+      {loading ? (
+        <IsLoading />
+      ) : error ? (
+        <p>error</p>
+      ) : (
+        <NeumourList listItems={songNames} />
+      )}
     </>
   );
 };
 
 const Page = () => {
   const searchParams = useSearchParams();
+  const auth = useAuth();
+  let userID = auth?.id;
 
-  const userID = auth.currentUser?.uid;
+  // クエリパラメータを取得
   const roomID = searchParams.get("roomID");
-  // const userID = searchParams.get("userID");
-  // ユーザーIDをstringとして出す
-  const numericUserID = userID || undefined;
-  const numericRoomID = roomID ? parseInt(roomID, 10) : undefined;
-
-  const { Song, RoomMembers } = useMusicRecommendPageData(
-    numericUserID,
-    numericRoomID
-  );
 
   // クエリがまだ利用できない場合のハンドリング
   if (!roomID || !userID) {
     return <IsLoading />;
   }
-
   if (typeof roomID !== "string" || typeof userID !== "string") {
     return <p>ルームIDまたはユーザーIDが不正です。</p>;
   }
-  if (numericUserID !== undefined && numericRoomID !== undefined) {
+  if (!roomID) {
     return (
-      <>
-        <ModalWhole userId={numericUserID} roomId={numericRoomID} />
-        <ShowRoomID roomID={String(numericRoomID)} />
-        {RoomMembers.loading ? (
-          <IsLoading />
-        ) : (
-          <>
-            <p>ルームネーム: {RoomMembers.data?.getMembers.roomName}</p>
-            <p>ユーザーリスト:</p>
-            <ul>
-              {RoomMembers.data?.getMembers.members?.map((member, i) => {
-                return <li key={i}>{member}</li>;
-              })}
-            </ul>
-          </>
-        )}
-        {Song.error ? (
-          <>
-            <p>曲のエラーが発生しました。やり直してください。</p>
-            <p>{Song.error.toString()}</p>
-          </>
-        ) : null}
-        {Song.loading ? <IsLoading /> : <SongList songsData={Song.data} />}
-      </>
+      <Suspense fallback={<IsLoading />}>
+        <SongList roomID={Number(roomID)} userID={userID} />
+      </Suspense>
     );
   }
+  return (
+    <Suspense fallback={<IsLoading />}>
+      <SongList roomID={Number(roomID)} userID={userID} />
+    </Suspense>
+  );
 };
 
 export default Page;
