51861e2827921f62d261d04f94f9067e2445f4fb
hiromuota166 <otahiromu166@gmail.com>
2023-12-08 00:40:53 +0900

    headerをトップページのリンクにした

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
+'use client'
 import GoogleLoginBtn from "./GoogleLoginBtn";
+
+const handleClick = () => {
+  window.location.href = '/';
+}
 
 const Header = () => {
   return (
     <header className="shadow-boxOut bg-background flex justify-between">
       <div>
-        <h1 className="p-4 pt-6 text-3xl text-font">MoodHub</h1>
+        <button>
+          <h1 
+            onClick={handleClick}
+            className="p-4 pt-6 text-3xl text-font">MoodHub
+          </h1>
+        </button>
       </div>
       <div className="items-center flex p-4">
         <GoogleLoginBtn />
