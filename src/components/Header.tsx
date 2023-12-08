7d8b9f8e260063a47881d7fd5d8c534b0a42b216
hiromuota166 <otahiromu166@gmail.com>
2023-12-08 09:47:13 +0900

    npm run format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,7 @@ const Header = () => {
     <header className="shadow-boxOut bg-background flex justify-between">
       <div>
         <h1 className="p-4 pt-6 text-3xl text-font">
-          <a href="/">
-          MoodHub
-          </a>
+          <a href="/">MoodHub</a>
         </h1>
       </div>
       <div className="items-center flex p-4">
