dead5973d5164ae1f9b8ed4e693b3827d6151000
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-12-27 17:10:22 +0900

    format

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -3,33 +3,31 @@ import ShowSpotify from "./ShowSpotify";
 import { Song } from "@/lib/apollo/gql/graphql";
 import IsLoading from "./IsLoading";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-} from '@chakra-ui/react'
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 
 const SongItem: React.FC<{ song: Song }> = ({ song }) => {
-    return (
-        <div>
-            <Accordion allowToggle>
-                <AccordionItem>
-                    <AccordionButton>
-                        <h2 className="text-2xl w-full">
-                            {song.songName}
-                        </h2>
-                        <AccordionIcon />
-                    </AccordionButton>
-                    <AccordionPanel pb={4}>
-                        <Suspense fallback={<IsLoading />}>
-                            <ShowSpotify trackId={song.trackId} />
-                        </Suspense>
-                    </AccordionPanel>
-                </AccordionItem>
-            </Accordion>
-        </div>
-    );
+  return (
+    <div>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <h2 className="text-2xl w-full">{song.songName}</h2>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Suspense fallback={<IsLoading />}>
+              <ShowSpotify trackId={song.trackId} />
+            </Suspense>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 };
 
 export default SongItem;
