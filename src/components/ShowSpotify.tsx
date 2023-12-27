c577117468294048fc91f42a1cf815b1abe89248
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-12-27 15:58:27 +0900

    merge develop

--- src/components/ShowSpotify.tsx
+++ src/components/ShowSpotify.tsx
@@ -9,12 +9,21 @@ const ShowSpotify = (props: ShowSpotifyProps) => {
     return <div>動画が見つかりません。</div>;
   }
 
-  const theme = "&theme=0"
+  const theme = "&theme=0";
 
   const videoUrl = `https://open.spotify.com/embed/track/${trackId}?utm_source=generator${theme}`;
 
   return (
-    <iframe style={{ borderRadius: "12px" }} src={videoUrl} width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" />
+    <iframe
+      style={{ borderRadius: "12px" }}
+      src={videoUrl}
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowFullScreen
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    />
   );
 };
 
--- src/lib/apollo/gql/gql.ts
+++ src/lib/apollo/gql/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from "./graphql";
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,8 +13,7 @@ import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  "\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  ":
-    types.CreateARoomDocument,
+    "\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  ": types.CreateARoomDocument,
 };
 
 /**
@@ -34,13 +33,10 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: "\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  "
-): (typeof documents)["\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  "];
+export function graphql(source: "\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  "): (typeof documents)["\n    mutation CreateARoom($room: CreateRoom!) {\n      createRoom(room: $room) {\n        roomId\n        userId\n        name\n      }\n    }\n  "];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
