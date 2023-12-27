6429b840a18a12bf9e10512b45d25ef8df6e431f
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-12-27 17:10:07 +0900

    アコーディオン導入

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -2,16 +2,34 @@ import React, { Suspense } from "react";
 import ShowSpotify from "./ShowSpotify";
 import { Song } from "@/lib/apollo/gql/graphql";
 import IsLoading from "./IsLoading";
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+} from '@chakra-ui/react'
 
 const SongItem: React.FC<{ song: Song }> = ({ song }) => {
-  return (
-    <div>
-      <h3>{song.songName}</h3>
-      <Suspense fallback={<IsLoading />}>
-        <ShowSpotify trackId={song.trackId} />
-      </Suspense>
-    </div>
-  );
+    return (
+        <div>
+            <Accordion allowToggle>
+                <AccordionItem>
+                    <AccordionButton>
+                        <h2 className="text-2xl w-full">
+                            {song.songName}
+                        </h2>
+                        <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel pb={4}>
+                        <Suspense fallback={<IsLoading />}>
+                            <ShowSpotify trackId={song.trackId} />
+                        </Suspense>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    );
 };
 
 export default SongItem;
