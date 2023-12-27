9071ecd0ea0cee115b8cc64b9a45b85ef654c78a
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-12-27 15:58:59 +0900

    format

--- src/lib/apollo/gql/graphql.ts
+++ src/lib/apollo/gql/graphql.ts
@@ -1,19 +1,32 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
 };
 
 export type CreateRoom = {
@@ -27,7 +40,7 @@ export type JoinRoom = {
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createRoom: Room;
   joinRoom: Room;
   register: RegisterComplete;
@@ -35,51 +48,43 @@ export type Mutation = {
   updateUsername: RegisterComplete;
 };
 
-
 export type MutationCreateRoomArgs = {
   room: CreateRoom;
 };
-
 
 export type MutationJoinRoomArgs = {
   join: JoinRoom;
 };
 
-
 export type MutationRegisterArgs = {
   regist: Register;
 };
 
-
 export type MutationUpdateCategoryArgs = {
   update: UpdateCategories;
 };
-
 
 export type MutationUpdateUsernameArgs = {
   update: UpdateUserName;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   getMembers: RoomMembers;
   getUserInfo: RegisterComplete;
   song: Array<Song>;
 };
 
-
 export type QueryGetMembersArgs = {
-  roomId: Scalars['Int']['input'];
+  roomId: Scalars["Int"]["input"];
 };
-
 
 export type QueryGetUserInfoArgs = {
   userId: Scalars["String"]["input"];
 };
 
-
 export type QuerySongArgs = {
-  roomId: Scalars['Int']['input'];
+  roomId: Scalars["Int"]["input"];
 };
 
 export type Register = {
@@ -107,16 +112,16 @@ export type Room = {
 };
 
 export type RoomMembers = {
-  __typename?: 'RoomMembers';
-  members: Array<Scalars['String']['output']>;
-  roomName: Scalars['String']['output'];
+  __typename?: "RoomMembers";
+  members: Array<Scalars["String"]["output"]>;
+  roomName: Scalars["String"]["output"];
 };
 
 export type Song = {
-  __typename?: 'Song';
-  categories: Array<Scalars['String']['output']>;
-  songName: Scalars['String']['output'];
-  trackId: Scalars['String']['output'];
+  __typename?: "Song";
+  categories: Array<Scalars["String"]["output"]>;
+  songName: Scalars["String"]["output"];
+  trackId: Scalars["String"]["output"];
 };
 
 export type UpdateCategories = {
