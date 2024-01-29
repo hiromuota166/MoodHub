3e49732e003a93c29995b4c58b1e6a540452b296
hiromuota166 <otahiromu166@gmail.com>
2024-01-29 11:56:06 +0900

    registerのエラー文確認済み

--- src/components/GoogleLoginBtn.tsx
+++ src/components/GoogleLoginBtn.tsx
@@ -3,6 +3,10 @@ import { useAuth } from "@/context/auth";
 import { login } from "@/lib/auth";
 import { useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
+import { useMutation } from "@apollo/client";
+import { useCallback } from "react";
+import { REGISTER_USER } from "../lib/apollo/apollo-query";
+import { Register, RegisterComplete } from "../lib/apollo/gql/graphql";
 
 type LoginState = "NOT_LOGGED_IN" | "LOGGING_IN" | "LOGGED_IN" | "LOGIN_FAILED";
 
@@ -15,12 +19,36 @@ export default function GoogleLoginBtn() {
     setLoginState(user ? "LOGGED_IN" : "NOT_LOGGED_IN");
   }, [user]);
 
+  const [registerUserFunc] = useMutation<{
+    register: RegisterComplete;
+  }>(REGISTER_USER);
+
+  const registerUserQuery = useCallback(
+    async (userData: Register) => {
+      await registerUserFunc({
+        variables: userData,
+      }).catch((err) => {
+        console.error(err);
+      });
+    },
+    [registerUserFunc]
+  );
+
   const signIn = () => {
     setLoginState("LOGGING_IN");
 
     login()
-      .then((userCrednetial) => {
+      .then(async (userCrednetial) => {
         const user = userCrednetial.user;
+
+        const userData: Register = {
+          userId: user.uid,
+          categories: [], // 必要に応じてカテゴリを設定
+          userName: user.displayName || null,
+          gender: null, // 性別が必要な場合は設定
+          age: null, // 年齢が必要な場合は設定
+        };
+        await registerUserQuery(userData);
         setLoginState("LOGGED_IN");
         console.log(user);
       })
