0a07391f94f9301f2bce885a7282c392fa131439
hiromuota166 <otahiromu166@gmail.com>
2024-02-03 00:08:14 +0900

    曲検索機能ずみ

--- src/hooks/useMusicRecommendPageData.ts
+++ src/hooks/useMusicRecommendPageData.ts
@@ -1,6 +1,4 @@
-// import { useCallback } from "react";
 import useApolloQuery from "@/lib/apollo/useApolloQuery";
-// import { Register, UpdateCategories } from "@/lib/apollo/gql/graphql";
 
 const useMusicRecommendPageData = (userID?: string, roomID?: number) => {
   const {
@@ -25,6 +23,8 @@ const useMusicRecommendPageData = (userID?: string, roomID?: number) => {
     }).catch((err) => {
       console.error(err);
     });
+    if (!roomID) return;
+    await Song.refetch();
   };
 
   return {
